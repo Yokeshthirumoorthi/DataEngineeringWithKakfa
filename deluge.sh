2e1f528d05966ba5a350ee74bfb3842a7d7b0c09
Yokesh <yokeshthirumoorthi@gmail.com>
2021-01-28 01:26:01 -0800

    add deluge sh file

---
+++ deluge.sh
@@ -0,0 +1,52 @@
+#! /bin/bash 
+# /usr/local/bin/deluge.sh 
+#
+ 
+function d_start() 
+{ 
+	echo  "Deluge: starting service" 
+    source ./confluent-exercise/bin/activate
+    ./consumer.py -f $HOME/.confluent/librdkafka.config -t test1
+	echo  "PID is" 
+}
+ 
+function d_stop()
+{ 
+	echo  "Deluge: stopping Service (PID = $ (cat /tmp/deluge.pid) )" 
+	kill $(cat /tmp/deluge.pid) 
+	rm /tmp/deluge.pid
+ }
+ 
+function d_status()
+{ 
+	ps -ef | grep deluged | grep -v grep
+	echo  "PID indicate indication file $(cat /tmp/deluge.pid 2> /dev/null)" 
+}
+ 
+# Some Things That run always 
+# touch  /var/lock/deluge
+ 
+# Management instructions of the service 
+case  "$1"  in 
+	start )
+		d_start
+		;; 
+	stop )
+		d_stop
+		;; 
+	reload )
+		d_stop
+		sleep  1
+		d_start
+		;; 
+	status )
+		d_status
+		;; 
+	* ) 
+	Echo  "Usage: $ 0 {start | stop | reload | status}" 
+	exit  1 
+	;; 
+esac
+ 
+exit  0
+
